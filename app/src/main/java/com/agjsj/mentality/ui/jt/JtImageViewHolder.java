3862b1e36fc02d0fb824255bb18b958ac5868f28
chaohengxing <396965650@qq.com>
2016-11-06 20:10:53 +0800

    鸡汤页面布局完成  逻辑有待补充

--- app/src/main/java/com/agjsj/mentality/ui/jt/JtImageViewHolder.java
+++ app/src/main/java/com/agjsj/mentality/ui/jt/JtImageViewHolder.java
@@ -1,6 +1,7 @@
 package com.agjsj.mentality.ui.jt;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -28,6 +29,14 @@ public class JtImageViewHolder extends BaseViewHolder {
     public void bindData(Object o) {
 
         String imageUrl = (String) o;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        //该属性设置为true只会加载图片的边框进来，并不会加载图片具体的像素点
+        options.inJustDecodeBounds = true;
+        //第一次加载图片，这时只会加载图片的边框进来，并不会加载图片中的像素点
+//        BitmapFactory.decode
+        //获得原图的宽和高
+        int outWidth = options.outWidth;
+        int outHeight = options.outHeight;
 
         PicassoUtils.loadImage(imageUrl, R.drawable.logo, R.drawable.logo, ivImage);
     }
